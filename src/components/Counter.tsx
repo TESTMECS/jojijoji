2b983f5a21790c45a2dfed218f016cc99aff23a1
Andrew Warkentin <axwar7410@gmail.com>
2024-06-23 10:13:19 -0400

    fixed typescript errors

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import Home from "./Home"
-import "../App.css"
-
+import React, { useState, useEffect } from "react";
+import Home from "./Home";
+import "../App.css";
 
 interface Props {
   delay1: number;
@@ -13,21 +12,18 @@ const Counter: React.FC<Props> = ({ delay1, delay2, delay3 }) => {
   const [count1, setCount1] = useState(0); // For the range 00-100
   const [count2, setCount2] = useState(0); // For the range 00-59
   const [count3, setCount3] = useState(0); // For the range 00-59
-  
-
-
   const [running1, setRunning1] = useState(true);
   const [running2, setRunning2] = useState(true);
   const [running3, setRunning3] = useState(true);
   const scrollHandler = () => {
     const viewportHeight = window.innerHeight;
-    const arrow: HTMLElement | null = document.getElementById("arrow"); 
+    const arrow: HTMLElement | null = document.getElementById("arrow");
     if (arrow) {
-        arrow.style.opacity = "0"; 
+      arrow.style.opacity = "0";
     }
     window.scrollBy({
       top: viewportHeight,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   };
 
@@ -38,7 +34,7 @@ const Counter: React.FC<Props> = ({ delay1, delay2, delay3 }) => {
 
     if (running1) {
       interval1 = setInterval(() => {
-        setCount1(prev => {
+        setCount1((prev) => {
           if (prev < 99) {
             return prev + 1;
           } else {
@@ -51,7 +47,7 @@ const Counter: React.FC<Props> = ({ delay1, delay2, delay3 }) => {
 
     if (running2) {
       interval2 = setInterval(() => {
-        setCount2(prev => {
+        setCount2((prev) => {
           if (prev < 59) {
             return prev + 1;
           } else {
@@ -64,7 +60,7 @@ const Counter: React.FC<Props> = ({ delay1, delay2, delay3 }) => {
 
     if (running3) {
       interval3 = setInterval(() => {
-        setCount3(prev => {
+        setCount3((prev) => {
           if (prev < 59) {
             return prev + 1;
           } else {
@@ -74,8 +70,6 @@ const Counter: React.FC<Props> = ({ delay1, delay2, delay3 }) => {
         });
       }, delay3);
     }
-    
-    
 
     return () => {
       if (interval1) clearInterval(interval1);
@@ -84,36 +78,33 @@ const Counter: React.FC<Props> = ({ delay1, delay2, delay3 }) => {
     };
   }, [delay1, delay2, delay3, running1, running2, running3]);
 
-
   return (
     <>
-    <main id="counter">
-  <div className="z-0 flex h-screen items-center justify-center text-9xl text-white">
-    <div> {count1.toString().padStart(2, '0')}</div>
-    <span> : </span>
-    <div> {count2.toString().padStart(2, '0')}</div>
-    <span> : </span>
-    <div> {count3.toString().padStart(2, '0')}</div>
-  </div>
-  </main>
-  <div>
-    {count1 === 100 && (
-      <div className="flex justify-center align-center">
-        <div id="arrow" onClick={scrollHandler} className='z-10 arrow fixed bottom-5'>
-         
+      <main id="counter">
+        <div className="z-0 flex h-screen items-center justify-center text-9xl text-white">
+          <div> {count1.toString().padStart(2, "0")}</div>
+          <span> : </span>
+          <div> {count2.toString().padStart(2, "0")}</div>
+          <span> : </span>
+          <div> {count3.toString().padStart(2, "0")}</div>
         </div>
-        <div> 
-          <Home /> 
-        </div>
+      </main>
+      <div>
+        {count1 === 100 && (
+          <div className="flex justify-center align-center">
+            <div
+              id="arrow"
+              onClick={scrollHandler}
+              className="z-10 arrow fixed bottom-5"
+            ></div>
+            <div>
+              <Home />
+            </div>
+          </div>
+        )}
       </div>
-        
-    )}
-  </div>
-  </>
-  
-);
-
-
+    </>
+  );
 };
 
-export default Counter
+export default Counter;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,137 +1,182 @@
-import { useState, useRef } from 'react';
-import '../App.css'
-import videoBg from '../assets/videos/dieforu.mp4';
-import ykVideo from '../assets/videos/ykon.mp4'; 
-import glimpseVideo from '../assets/videos/Glimpseofus.mp4';
-import tikVideo from '../assets/videos/ticktock.mp4';
-import upgradeVideo from '../assets/videos/upgrade.mp4';
+import { useState, useRef } from "react";
+import "../App.css";
+import videoBg from "../assets/videos/dieforu.mp4";
+import ykVideo from "../assets/videos/ykon.mp4";
+import glimpseVideo from "../assets/videos/Glimpseofus.mp4";
+import tikVideo from "../assets/videos/ticktock.mp4";
+import upgradeVideo from "../assets/videos/upgrade.mp4";
 
 const Home = () => {
-    const [videoSrc, setVideoSrc] = useState(videoBg);
-    const [isMuted, setIsMuted] = useState(true);
-    const [showControls, setShowControls] = useState(false);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const videoRef = useRef(null);
+  const [videoSrc, setVideoSrc] = useState<string>(videoBg);
+  const [isMuted, setIsMuted] = useState<boolean>(false);
+  const [showControls, setShowControls] = useState<boolean>(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-
-    
-    const handleVolumeChange = (event) => {
-        const volume = event.target.value;
-        videoRef.current.volume = volume;
-
+  const handleVolumeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const volume = parseFloat(event.target.value);
+    if (videoRef.current) {
+      videoRef.current.volume = volume;
     }
+  };
 
-    const handleProgress = () => {
-        const video = videoRef.current;
-        const progressBar = document.getElementById("progressbar");
-        progressBar.value = (video.currentTime / video.duration) * 100;
+  const handleProgress = () => {
+    const video = videoRef.current;
+    const progressBar = document.getElementById(
+      "progressbar"
+    ) as HTMLInputElement;
+    if (video && progressBar) {
+      progressBar.value = (
+        (video.currentTime / video.duration) *
+        100
+      ).toString();
     }
+  };
 
-    const handleSeek = (event) => {
-        const video = videoRef.current;
-        video.currentTime = (event.target.value / 100) * video.duration;
+  const handleSeek = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const video = videoRef.current;
+    if (video) {
+      video.currentTime =
+        (parseFloat(event.target.value) / 100) * video.duration;
     }
+  };
 
-    const toggleFullscreen = () => {
-        const video  = videoRef.current;
-        if (video.requestFullscreen){
-            video.requestFullscreen();
-        } else if (video.webkitRequestFullscreen){
-            video.webkitRequestFullscreen();
-        } else if (video.msRequestFullscreen) {
-            video.msRequestFullscreen();
-        }
+  const toggleFullscreen = () => {
+    const video = videoRef.current;
+    if (video) {
+      if (video.requestFullscreen) {
+        video.requestFullscreen();
+      }
     }
+  };
 
-    
-    const handleInvert = () => {
-        const video = document.getElementById("mainvideo");
-        if (!video) return;  // If the video element is not found, do nothing
-    
-        const currentFilter = video.style.webkitFilter || "";
-    
-        if (currentFilter.includes("invert(100%)") || !currentFilter) {
-            video.style.webkitFilter = "invert(0%)";
-        } else {
-            video.style.webkitFilter = "invert(100%)";
-        }
+  const handleInvert = () => {
+    const video = document.getElementById("mainvideo") as HTMLVideoElement;
+    if (!video) return;
+
+    const currentFilter = video.style.webkitFilter || "";
+
+    if (currentFilter.includes("invert(100%)") || !currentFilter) {
+      video.style.webkitFilter = "invert(0%)";
+    } else {
+      video.style.webkitFilter = "invert(100%)";
     }
+  };
 
-
-    const togglePlayPause = () => {
-        setShowControls(false)
-        const video = document.getElementById("mainvideo");
-        if (video?.paused || video?.ended) {
-            video.play();
-            setIsPlaying(true);
-        } else {
-            video?.pause();
-            setIsPlaying(false);
-        }
+  const togglePlayPause = () => {
+    setShowControls(false);
+    const video = document.getElementById("mainvideo") as HTMLVideoElement;
+    if (video) {
+      if (video.paused || video.ended) {
+        video.play();
+      } else {
+        video.pause();
+      }
     }
-    
-        
-    const handleVideoChange = (source: string) => {
-        setVideoSrc(source);
-        setIsMuted(false);
-        setShowControls(false);
-    }
+  };
 
-    return (
-        <div id="Home" className=" w-screen h-screen">
-            <video 
-                id="mainvideo" 
-                className="z-20 w-screen h-screen object-cover" 
-                src={videoSrc} 
-                autoPlay 
-                muted={isMuted} 
-                loop 
-                controls={showControls}
-                ref={videoRef}
-                onTimeUpdate={handleProgress}
-                
-            />
-            <header className="absolute top-[100vh]">
-                <h1 className="text-5xl"><strong>Joji</strong> SMITHEREENS OUT NOW </h1>
-                <div className="flex space-x-10 text-5xl">
-                    <a href="https://jojimusic.com/" target="_blank" rel="noreferrer"><h1> Tour </h1></a>
-                    <a href="https://shop.jojimusic.com/" target="_blank" rel="noreferrer"><h1> Shop </h1></a>
-                    <button type="button" onClick={handleInvert}> Invert </button>
-                </div>
-            </header>
-            <div id="yukontext" className='absolute top-[180vh] right-10'>
-                <button onClick={ () => handleVideoChange(ykVideo)}>
-                    <h1>Yukon's Interlude </h1>
-                    <p> 3:28 </p>
-                </button>
-            </div>
-            <div className='absolute top-[180vh] left-10'>
-                <button onClick= { () => handleVideoChange(glimpseVideo)}>
-                    <h1> Glimpse of Us</h1>
-                    <p> 3:54 </p>
-                </button>
-            </div>
-            <div className='absolute top-[180vh] left-[500px]'>
-                <button onClick= { () => handleVideoChange(tikVideo)}>
-                    <h1> Tick Tock </h1>
-                    <p> 2:12 </p>
-                </button>
-            </div>
-            <div className='absolute top-[180vh] left-[300px]'>
-                <button onClick= { () => handleVideoChange(upgradeVideo)}>
-                    <h1> Upgrade </h1>
-                    <p> 1:35 </p>
-                </button>
-            </div>
-            {/*Player */}
-            <div className='flex'>
-            <button id="play" className="absolute top-[195vh] left-[100px] text-white" onClick={togglePlayPause}> Play/Pause </button>
-            <input id="progressbar" className='absolute top-[195vh] left-[200px]' type="range" min="0" max="100" step="0.1" onChange={handleSeek} />
-            <input id='volcontrol' className='absolute top-[183vh] ' type="range" min="0" max="1" step="0.01" onChange={handleVolumeChange} />
-            <button id="fullscreenBtn" className='absolute top-[195vh] right-10' onClick={toggleFullscreen}> Fullscreen </button>
-            </div>
+  const handleVideoChange = (source: string) => {
+    setVideoSrc(source);
+    setIsMuted(false);
+    setShowControls(false);
+  };
+
+  return (
+    <div id="Home" className=" w-screen h-screen">
+      <video
+        id="mainvideo"
+        className="z-20 w-screen h-screen object-cover"
+        src={videoSrc}
+        autoPlay
+        muted={isMuted}
+        loop
+        controls={showControls}
+        ref={videoRef}
+        onTimeUpdate={handleProgress}
+      />
+      <header className="absolute top-[100vh]">
+        <h1 className="text-5xl">
+          <strong>Joji</strong> SMITHEREENS OUT NOW{" "}
+        </h1>
+        <div className="flex space-x-10 text-5xl">
+          <a href="https://jojimusic.com/" target="_blank" rel="noreferrer">
+            <h1> Tour </h1>
+          </a>
+          <a
+            href="https://shop.jojimusic.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h1> Shop </h1>
+          </a>
+          <button type="button" onClick={handleInvert}>
+            {" "}
+            Invert{" "}
+          </button>
         </div>
-    )
-}
+      </header>
+      <div id="yukontext" className="absolute top-[180vh] right-10">
+        <button onClick={() => handleVideoChange(ykVideo)}>
+          <h1>Yukon's Interlude </h1>
+          <p> 3:28 </p>
+        </button>
+      </div>
+      <div className="absolute top-[180vh] left-10">
+        <button onClick={() => handleVideoChange(glimpseVideo)}>
+          <h1> Glimpse of Us</h1>
+          <p> 3:54 </p>
+        </button>
+      </div>
+      <div className="absolute top-[180vh] left-[500px]">
+        <button onClick={() => handleVideoChange(tikVideo)}>
+          <h1> Tick Tock </h1>
+          <p> 2:12 </p>
+        </button>
+      </div>
+      <div className="absolute top-[180vh] left-[300px]">
+        <button onClick={() => handleVideoChange(upgradeVideo)}>
+          <h1> Upgrade </h1>
+          <p> 1:35 </p>
+        </button>
+      </div>
+      {/*Player */}
+      <div className="flex">
+        <button
+          id="play"
+          className="absolute top-[195vh] left-[100px] text-white"
+          onClick={togglePlayPause}
+        >
+          {" "}
+          Play/Pause{" "}
+        </button>
+        <input
+          id="progressbar"
+          className="absolute top-[195vh] left-[200px]"
+          type="range"
+          min="0"
+          max="100"
+          step="0.1"
+          onChange={handleSeek}
+        />
+        <input
+          id="volcontrol"
+          className="absolute top-[183vh] "
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          onChange={handleVolumeChange}
+        />
+        <button
+          id="fullscreenBtn"
+          className="absolute top-[195vh] right-10"
+          onClick={toggleFullscreen}
+        >
+          {" "}
+          Fullscreen{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
